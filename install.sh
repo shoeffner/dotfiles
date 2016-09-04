dd0c4214049f76cd18bd3534c04f4c74780b598d
Sebastian Höffner <info@sebastian-hoeffner.de>
2016-09-04 13:30:42 -0700

    setting up gpg signing

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # better not run this file as of now, just see it as advice on what to do
+return 
 
 cd ~
 # brew
@@ -10,9 +11,10 @@ brew upgrade
 # brew formulae and cask formulae
 brew install zsh cask macvim git python python3 thefuck wget ccat bash \
     bash-completion pandoc swi-prolog wget cmake boost htop-osx node \
-    go keybase neovim/neovim/neovim
+    go keybase neovim/neovim/neovim gpg
 brew cask install amethyst mactex haskell-platform java steam docker \
     xquartz
+brew tap caskroom/versions
 
 # npm installs
 npm i -g eslint estraverse estraverse-fb eslint-plugin-react babel-eslint
@@ -40,3 +42,24 @@ git clone git://github.com/mbrubeck/compleat.git
 
 # TODO: link files in this project to their respective counterparts
 # e.g. ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
+
+# git settings:
+git config --global color.diff.old "red strike"
+git config --global color.diff.new "green"
+
+
+# add gpg key from keybase to gpg and set it as the default for git
+# keybase export needs utf-8
+echo charset utf-8 >> ~/.gnupg/gpg.conf
+keybase pgp export | gpg --import
+keybase pgp export --secret | gpg --import --allow-secret-key-import
+
+# trust your secret key ultimately (also trusts your public key) - just type 
+# trust\n 5\n y\n quit\n
+gpg --edit-key $(gpg --list-secret-keys | grep ^sec | cut -d ' '  -f 4 | cut -d '/' -f 2)
+# set default gpg key
+echo default-key $(gpg --list-secret-keys | grep ^sec | cut -d ' '  -f 4 | cut -d '/' -f 2) >> ~/.gnupg/gpg.conf
+git config --global user.signingkey $(gpg --list-secret-keys | grep ^sec | cut -d ' '  -f 4 | cut -d '/' -f 2)
+
+# to auto sign set in you git repo:
+git config --local commit.gpgsign true
